f58f8c29eeed2a99c63a73bed6be62b8d514b127
shir21-meet <shir21@meet.mit.edu>
2019-07-21 11:29:34 -0400

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,23 @@
+
+import turtle
+turtle.penup() 
+turtle.goto(-200,-100)  
+turtle.pendown()
+turtle.goto(-200,-100+200) 
+turtle.goto(-200+50,-100) 
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.up()
+turtle.forward(45)
+turtle.down()
+turtle.left(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+turtle.backward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.mainloop()
+
